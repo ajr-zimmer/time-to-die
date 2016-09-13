dfa4cfbf1682de04805291150d12f1fcb5d9f322
ajr-zimmer <ajr.zimmer@gmail.com>
2016-09-13 00:03:50 -0600

    Shared Preferences working for storing age, next steps country selector

--- app/src/main/java/com/ttd/cain/timetodie/MainActivity.java
+++ app/src/main/java/com/ttd/cain/timetodie/MainActivity.java
@@ -1,32 +1,63 @@
 package com.ttd.cain.timetodie;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     public final static String EXTRA_MESSAGE = "com.ttd.cain.timetodie.MESSAGE";
 
+    EditText yearsAlive;
+    TextView yearsShown;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        yearsAlive = (EditText) findViewById(R.id.years_alive);
+        yearsShown = (TextView) findViewById(R.id.years_shown);
+
     }
 
-    /** Called when the user clicks the Send button */
-    public void sendMessage(View view){
+    /** Saves information in the text field on the current activity */
+    public void saveYears(View view){
+        SharedPreferences sharedPref = getSharedPreferences("userInfo", Context.MODE_PRIVATE);
+
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString("userage", yearsAlive.getText().toString());
+        editor.apply();
+
+        Toast.makeText(this, "Thanks for that ;)", Toast.LENGTH_LONG).show();
+    }
+
+    /** Displays information in the text field on the current activity */
+    public void showYears(View view){
+        SharedPreferences sharedPref = getSharedPreferences("userInfo", Context.MODE_PRIVATE);
+
+        String age = sharedPref.getString("userage", "");
+        yearsShown.setText(age);
+    }
+
+    /** Sends information in text field to the next activity for display */
+    public void submitYears(View view){
         Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.edit_message);
+        EditText editText = (EditText) findViewById(R.id.years_alive);
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
     }
 
+
+    // fun little method for toasts
     public void onButtonTap(View v){
-        EditText editText = (EditText) findViewById(R.id.edit_message);
+        EditText editText = (EditText) findViewById(R.id.years_alive);
         String message = editText.getText().toString();
         Toast myToast = Toast.makeText(
                 getApplicationContext(),
